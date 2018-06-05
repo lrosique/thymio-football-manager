7bf67d070da941ac079c535ac37125af6062addb
lrosique <lambertrosique@hotmail.com>
2018-06-05 14:28:13 +0200

    code

--- scanner_raspberry_thymio.py
+++ scanner_raspberry_thymio.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-import utils.detectionutils as du
-import utils.detectionparameters as p
+import utils.detection_utils as du
+import utils.detection_parameters as p
 import shutil
 import redis
 import pickle
@@ -56,30 +56,32 @@ def get_football_field():
     total_results, positions,angles, results = du.analyse_all_fields(angles,positions,p.hsv_green,p.hsv_rose,p.hsv_blue,p.parameters_thymio_ld,p.parameters_dots_ld,save_img)
     return total_results,results
 
-print("Scanner loaded... and loop started.")
-
-start = r.get("start")
-while True:
-    if r.get("start") != None and r.get("start").decode("utf-8") != start.decode("utf-8"):
-        print("Server started !") if start.decode("utf-8") == "0" else print("Server stopped !")
+def loop_scanner_redis():
+    global positions,crops,angles,total_results,results
     start = r.get("start")
-    calibrate_fields = r.get("calibrate_fields")
-    calibrate_image = r.get("calibrate_image")
-    reset = r.get("reset")
-    if calibrate_fields != None and calibrate_fields.decode("utf-8") == '1':
-        print("Début de calibration des terrains")
-        get_calibrate_football_fields()
-        r.set('crops',pickle.dumps(crops))
-        r.set("calibrate_fields",'0')
-        print("Calibration des terrains terminée")
-    if calibrate_image != None and calibrate_image.decode("utf-8") == '1':
-        print("Début de calibration de l'image")
-        get_calibrate_image()
-        r.set('crops',pickle.dumps(crops))
-        r.set("calibrate_image",'0')
-        print("Calibration de l'image terminée")
-    if start != None and start.decode("utf-8") == '1':
-        get_calibrate_image()
-        get_football_field()
-        r.set('total_results',pickle.dumps(total_results))
-        r.set('results',pickle.dumps(results))
+    while True:
+        if r.get("start") != None and r.get("start").decode("utf-8") != start.decode("utf-8"):
+            print("Server started !") if start.decode("utf-8") == "0" else print("Server stopped !")
+        start = r.get("start")
+        calibrate_fields = r.get("calibrate_fields")
+        calibrate_image = r.get("calibrate_image")
+        if calibrate_fields != None and calibrate_fields.decode("utf-8") == '1':
+            print("Début de calibration des terrains")
+            get_calibrate_football_fields()
+            r.set('crops',pickle.dumps(crops))
+            r.set("calibrate_fields",'0')
+            print("Calibration des terrains terminée")
+        if calibrate_image != None and calibrate_image.decode("utf-8") == '1':
+            print("Début de calibration de l'image")
+            get_calibrate_image()
+            r.set('crops',pickle.dumps(crops))
+            r.set("calibrate_image",'0')
+            print("Calibration de l'image terminée")
+        if start != None and start.decode("utf-8") == '1':
+            get_calibrate_image()
+            get_football_field()
+            r.set('total_results',pickle.dumps(total_results))
+            r.set('results',pickle.dumps(results))
+
+print("Scanner loaded... and loop started.")
+loop_scanner_redis()
