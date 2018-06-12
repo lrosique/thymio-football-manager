486898161e4e274b0114f765b2eea985bf911aaa
lrosique <lambertrosique@hotmail.com>
2018-06-12 15:16:10 +0200

    cc

--- thymio_python/pithymio.py
+++ thymio_python/pithymio.py
@@ -1,82 +1,60 @@
 import dbus
 import dbus.mainloop.glib
-##import gobject
-from optparse import OptionParser
+from gi.repository import GObject
+
+# sudo apt install python-gi python-gi-cairo python3-gi python3-gi-cairo gir1.2-gtk-3.0
 
 proxSensorsVal=[0,0,0,0,0]
 
-
 class Thymio:
-    motorLeft
-    motorRight
-    proxSensorsVal
+    motorLeft=0
+    motorRight=0
+    proxSensorsVal=[0,0,0,0,0]
 
     def __init__(self, nom='Thymio'):
         self.nom = nom
 
-
-
-    def connect(port):
-
-
-
-    def setMotorLeft(motor_left):
+    def setMotorLeft(self,motor_left):
         network.SetVariable("thymio-II", "motor.left.target", [motor_left])
-        motorLeft = motor_left
+        self.motorLeft = motor_left
 
-    def setMotorRight(motor_right):
+    def setMotorRight(self,motor_right):
         network.SetVariable("thymio-II", "motor.right.target", [motor_right])
-        motorRight = motor_right
+        self.motorRight = motor_right
 
-    def setMotors(motor_left, motor_right):
-        setMotorLeft(motor_left)
-        setMotorRight(motor_right)
+    def setMotors(self,motor_left, motor_right):
+        self.setMotorLeft(motor_left)
+        self.setMotorRight(motor_right)
 
 
-    def get_proxSensors():
+    def get_proxSensors(self):
         network.GetVariable("thymio-II", "prox.horizontal",reply_handler=get_variables_reply,error_handler=get_variables_error)
 
-
-
-def get_variables_reply(r):
+def get_variables_reply2(r):
     global proxSensorsVal
     proxSensorsVal=r
+    print('rrrrrrrrrrr')
+    print(r)
+    return r
 
 def get_variables_error(e):
     print('error:')
     print(str(e))
-    loop.quit()
 
-print("toot")
-
-def __print--:
-    return "xx"
-
-def --add--:
-
+def Braitenberg():
+    network.GetVariable("thymio-II", "prox.horizontal",reply_handler=get_variables_reply2,error_handler=get_variables_error)
+    print(proxSensorsVal[0],proxSensorsVal[1],proxSensorsVal[2],proxSensorsVal[3],proxSensorsVal[4])
+    return True
 
 if __name__ == '__main__':
-#    parser = OptionParser()
-#    parser.add_option("-s", "--system", action="store_true", dest="system", default=False,help="use the system bus instead of the session bus")
-#
-#    (options, args) = parser.parse_args()
-
     dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
-
-    if options.system:
-        bus = dbus.SystemBus()
-    else:
-        bus = dbus.SessionBus()
+    bus = dbus.SessionBus()
 
     #Create Aseba network
     network = dbus.Interface(bus.get_object('ch.epfl.mobots.Aseba', '/'), dbus_interface='ch.epfl.mobots.AsebaNetwork')
 
-    #print in the terminal the name of each Aseba NOde
-    print(network.GetNodesList())
-
-    #GObject loop
-    print('starting loop')
-    loop = gobject.MainLoop()
-    #call the callback of Braitenberg algorithm
-    handle = gobject.timeout_add (100, Braitenberg) #every 0.1 sec
+    thymio = Thymio()
+    thymio.setMotors(100,100)
+    loop = GObject.MainLoop()
+    handle = GObject.timeout_add (1000, Braitenberg)
     loop.run()
